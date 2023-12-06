8108b07db954c1f68c9c1291b2183829ec050090
Dan <dwnorm2@gmail.com>
2023-12-06 12:56:31 -0600

    refactor

--- js/main.js
+++ js/main.js
@@ -2,60 +2,68 @@
 // //for weather icons: https://developer.accuweather.com/weather-icons
 // //show weathertext and weathericon
 
+// Declare variables
 let realFeel, precipitation;
 
-function weather() {
+// Fetch weather data and update the DOM
+function fetchWeather() {
   const apiKey = 'FTr7D9KwDJzIbFpLNTVAJyWnAhMMfb4H';
+  const apiUrl = `https://dataservice.accuweather.com/currentconditions/v1/332667?apikey=${apiKey}&language=en-US&details=true`;
 
-  return new Promise((resolve, reject) => {
-    fetch(
-      `https://dataservice.accuweather.com/currentconditions/v1/332667?apikey=${apiKey}&language=en-US&details=true`
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data[0]);
-        document.getElementById('temp').textContent =
-          data[0].Temperature.Imperial.Value + 'f';
-        document.getElementById('precipitation').textContent =
-          data[0].HasPrecipitation;
-        document.getElementById('RealFeel').textContent = `${
-          data[0].RealFeelTemperature.Imperial.Value
-        }f (${data[0].RealFeelTemperature.Imperial.Phrase.toLowerCase()})`;
-        realFeel = data[0].RealFeelTemperature.Imperial.Value;
-        precipitation = data[0].HasPrecipitation;
-        resolve({ realFeel, precipitation });
-      })
-      .catch((err) => {
-        console.error(`Error: ${err.message}`);
-        reject(err);
-      });
-  });
+  return fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const weatherData = data[0];
+      updateDOM(weatherData);
+      return {
+        realFeel: weatherData.RealFeelTemperature.Imperial.Value,
+        precipitation: weatherData.HasPrecipitation,
+      };
+    })
+    .catch((err) => {
+      console.error(`Error: ${err.message}`);
+      throw err;
+    });
 }
 
-let body = document.querySelector('body');
-let playgroundImg = document.getElementById('outdoor');
-let classroomImg = document.getElementById('indoor');
+// Update the DOM with weather information
+function updateDOM(data) {
+  document.getElementById(
+    'temp'
+  ).textContent = `${data.Temperature.Imperial.Value}f`;
+  document.getElementById('precipitation').textContent = data.HasPrecipitation;
+  document.getElementById('RealFeel').textContent = `${
+    data.RealFeelTemperature.Imperial.Value
+  }f (${data.RealFeelTemperature.Imperial.Phrase.toLowerCase()})`;
+}
 
-weather()
-  .then(({ realFeel, precipitation }) => {
-    // precipitation = true;
-    // realFeel = 1;
-    if (realFeel >= 90 || realFeel < 30) {
-      body.style.backgroundColor = 'yellow';
-      classroomImg.style.display = 'block';
-      playgroundImg.style.display = 'block';
-    }
-    if (precipitation === true || realFeel < 9 || realFeel > 95) {
-      body.style.backgroundColor = 'red';
-      playgroundImg.style.display = 'none';
-      classroomImg.style.display = 'block';
-    }
-  })
+// Apply weather conditions to the UI
+function applyWeatherConditions({ realFeel, precipitation }) {
+  let body = document.querySelector('body');
+  let playgroundImg = document.getElementById('outdoor');
+  let classroomImg = document.getElementById('indoor');
+
+  if (realFeel >= 90 || realFeel < 30) {
+    body.style.backgroundColor = 'yellow';
+    classroomImg.style.display = 'block';
+    playgroundImg.style.display = 'block';
+  }
+
+  if (precipitation === true || realFeel < 9 || realFeel > 95) {
+    body.style.backgroundColor = 'red';
+    playgroundImg.style.display = 'none';
+    classroomImg.style.display = 'block';
+  }
+}
+
+// Fetch weather and apply conditions
+fetchWeather()
+  .then(applyWeatherConditions)
   .catch((error) => {
     console.error('Error:', error);
   });
